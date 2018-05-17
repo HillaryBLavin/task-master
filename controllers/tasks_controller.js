98730d364a5e4275b608ba63bf5a7303a6cf8c06
Hillary Lavin <hilavin@gmail.com>
2018-05-17 13:07:24 -0400

    Render task_data to homepage

--- controllers/tasks_controller.js
+++ controllers/tasks_controller.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var task = require('../models/burger.js');
+var task = require('../models/task.js');
 
 router.get('/', function(req, res) {
     task.all(function(task_data) {
         console.log(task_data);
-        res.render('index');
+        res.render('index', {task_data});
     })
     
 })
